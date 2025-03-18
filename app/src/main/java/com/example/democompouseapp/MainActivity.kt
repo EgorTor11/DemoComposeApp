6ccc337fe2382c26efe6d4385b6ae85956d188f4
Egor Taranov <taranovegor91@gmail.com>
2025-03-19 00:00:33 +0300

    //

--- app/src/main/java/com/example/democompouseapp/MainActivity.kt
+++ app/src/main/java/com/example/democompouseapp/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             val mainViewModel = hiltViewModel<MainViewModel>()
-            MainScreen(mainViewModel)
+            MainScreen(mainViewModel) //
         }
     }
 }
